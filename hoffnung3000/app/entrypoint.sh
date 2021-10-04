0acafa4ff35d9680638f52e8f45d104cb2ff4603
Andreas Dzialocha <x12@adz.garden>
2021-10-04 09:44:06 +0200

    Add HOFFNUNG 3000

---
+++ hoffnung3000/app/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+CMD="$@"
+
+# Set default values
+DATABASE_DIALECT=postgres
+POSTGRES_PORT=${POSTGRES_PORT:-5432}
+NODE_ENV=${NODE_ENV:-"production"}
+
+# Wait until database is ready
+./wait-for-it.sh "$POSTGRES_HOST:$POSTGRES_PORT" -t 60
+
+# Set DATABASE_URL env variable in correct format for application
+export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB
+
+# Run migrations
+npm run db:migrate
+npm run db:seed
+
+# Finally execute start command
+exec $CMD
